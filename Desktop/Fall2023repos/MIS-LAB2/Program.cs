0b715d5bbbdb9118a180bf7b86460820c8ba33a4
jwcook4 <jwcook4@crimson.ua.edu>
2023-09-16 18:13:31 -0500

    Lab2

---
+++ Desktop/Fall2023repos/MIS-LAB2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Main
+
+string season = GetSeason();
+
+string message = FavoriteSeasonMessage(season);
+
+Console.WriteLine(message);
+
+// End Main
+
+static string GetSeason()
+{
+    Console.WriteLine("Enter the current season (Spring, Fall, or Winter)");
+    string userInput = Console.ReadLine();
+    return(userInput);
+}
+
+
+
+    static string FavoriteSeasonMessage(string season)
+    {
+        string message;
+        
+        if (season == "Fall")
+    
+        {
+            message = "Sherlenthia's favorite season is Fall!!!";
+        }
+        else if (season == "Winter")
+        {
+            message = "Manav's favorite season is Winter!";
+        }
+        else 
+        {
+            message = "It is not a TA's favorite season, you are alone";
+        }
+         return message;
+    }
